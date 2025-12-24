4035abf6f7100268b57f598f0fabbb63267b958c
Ethan-des <ethanburnesMKDM@gmail.com>
2025-12-24 14:09:30 -0800

    Update GridManager.cs

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -32,6 +32,7 @@ public class GridManager : MonoBehaviour
             }
         }
 
-        _cam.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -20);
+        //Determines the position of the camera
+        _cam.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, 10);
     }
 }
