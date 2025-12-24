9a29035bf5f774dc5424998792663955a3846000
Ethan-des <ethanburnesMKDM@gmail.com>
2025-12-24 14:45:00 -0800

    m

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -6,11 +6,19 @@ public class Tile : MonoBehaviour
 {
     //Changes tile's color based on code from GridManager
     [SerializeField] private Color _baseColor, _offsetColor;
+    //[SerializeField] private Color _offsetColor;
     [SerializeField] private SpriteRenderer _renderer;
 
     public void Init(bool isOffset){
+       
+        if (_renderer == null)
+            _renderer = GetComponent<SpriteRenderer>();
+       
+
         Color c = isOffset ? _offsetColor : _baseColor;
         c.a = 1f; // Tiles are fully opaque
         _renderer.color = c;
+
+        Debug.Log($"{name} | isOffset={isOffset} | color={c}");
     }
 }
