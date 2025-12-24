0d4e73322bb147826aea2c1a4f8a90630ac0e50f
Ethan-des <ethanburnesMKDM@gmail.com>
2025-12-24 15:38:38 -0800

    Adjusted grid to make it vertical

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -13,12 +13,16 @@ public class GridManager : MonoBehaviour
     //reference to the game's camera
     [SerializeField] private Transform _cam;
 
+    //To store and get the tiles
+    //private Dictionary<Vector2, Tile> _tiles;
+
     void Start(){
         Debug.Log("Generating grid");
         GenerateGrid();
     }
 
     void GenerateGrid(){
+
         for (int x = 0; x < _width; x++){
             for (int y = 0; y < _height; y++){
                 var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y, 0), Quaternion.identity);
@@ -29,10 +33,25 @@ public class GridManager : MonoBehaviour
                 //Is x even and y odd or is y odd and x even?
                 var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
                 spawnedTile.Init(isOffset);
+
+                //To get the tiles onto the scene
+                //_tiles[new Vector2(x, y)] = spawnedTile;
             }
         }
 
         //Determines the position of the camera
-        _cam.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -10);
+        _cam.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -20);
     }
+
+    /*
+    public Tile GetTileAtPosition(Vector2 pos)
+    {
+        if(_tiles.TryGetValue(pos, out var tile))
+        {
+            return tile;
+        }
+
+        return null;
+    }
+    */
 }
